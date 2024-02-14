dd930ab216e52a980389e7a80663614124bde401
Sogon-Njugush <josephnjuguna00@gmail.com>
2024-02-14 17:20:36 +0300

    disable eslint rules

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
